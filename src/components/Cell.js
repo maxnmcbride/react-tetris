45a35430d55bcf2bd489a0cd311cddcb9e960295
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-03-15 17:05:11 -0400

    styling components for game

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <div>cell</div>
+    <StyledCell type={type} color={TETROMINOS[type].color}>cell</StyledCell>
 )
 export default Cell;
