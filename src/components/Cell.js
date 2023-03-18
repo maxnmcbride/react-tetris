e80f1928b6271af37f19bba5759f82a803757aca
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-03-18 09:12:26 -0400

    continued styling on gaming components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,6 +3,6 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={type} color={TETROMINOS[type].color}>cell</StyledCell>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color} />
 )
 export default Cell;
