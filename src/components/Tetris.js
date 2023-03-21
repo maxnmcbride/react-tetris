4b3132cdaffd617d42fdf51164f43e8a5ecc9ba9
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-03-21 16:06:29 -0400

    up to date

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -14,50 +14,60 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player, set] = usePlayer();
     const [stage, setStage] = useStage(player);
 
     console.log('re-render')
 
     const movePlayer = dir => {
-
+        updatePlayerPos({ x: dir, y: 0 })
     }
 
     const startGame = () => {
-
+        // reset everything
+        setStage(createStage());
+        resetPlayer();
     }
 
     const drop = () => {
-
+        updatePlayerPos({ x: 0, y: 1, collided: false })
     }
 
     const dropPlayer = () => {
-
+        drop();
     }
 
-    const move = ({keyCode}) => {
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer()
+            }
+        }
 
+        return (
+            <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
+                <StyledTetris>
+                    <Stage stage={stage} />
+                    <aside>
+                        {gameOver ? (
+                            <Display gameOver={gameOver} text="Game Over" />
+                        ) : (
+                            <div>
+                                <Display text="Score" />
+                                <Display text="Rows" />
+                                <Display text="Level" />
+                            </div>
+                        )}
+                        <StartButton onClick={startGame} />
+                    </aside>
+                </StyledTetris>
+            </StyledTetrisWrapper>
+        )
     }
-
-    return (
-        <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
-            <StyledTetris>
-                <Stage stage={stage} />
-                <aside>
-                    {gameOver ? (
-                        <Display gameOver={gameOver} text="Game Over" />
-                    ) : (
-                        <div>
-                            <Display text="Score" />
-                            <Display text="Rows" />
-                            <Display text="Level" />
-                        </div>
-                    )}
-                    <StartButton />
-                </aside>
-            </StyledTetris>
-        </StyledTetrisWrapper>
-    )
 }
 
 export default Tetris;
